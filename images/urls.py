6836d207f89410fbdfd8aa7e806acff5e1bd64b6
Irina-Nazarova <Irina.g.nazarova@yandex.ru>
2021-04-04 12:22:22 +0300

    add urls.py

---
+++ images/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('create/', views.image_create, name='image_create'),
+    path('edit/<int:id>/', views.image_edit, name='image_edit'),
+]
