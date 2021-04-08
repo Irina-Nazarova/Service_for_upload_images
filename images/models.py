7c4f3e1eb67f657ba2a385b35d0c324d4e9c8cb6
Irina-Nazarova <Irina.g.nazarova@yandex.ru>
2021-04-08 22:21:30 +0300

    added models

--- images/models.py
+++ images/models.py
@@ -1,3 +1,24 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='',
+                                   blank=True,
+                                   null=True,
+                                   verbose_name='Файл изображения', )
+    width = models.IntegerField(
+        blank=True, null=True,
+        validators=[MinValueValidator(limit_value=0),
+                    MaxValueValidator(limit_value=5000,
+                                      message="Максимально возможная ширина - 5000", )],
+        verbose_name='Ширина')
+    height = models.IntegerField(
+        blank=True, null=True,
+        validators=[MinValueValidator(limit_value=0),
+                    MaxValueValidator(limit_value=5000,
+                                      message="Максимально возможная высота - 5000", )],
+        verbose_name='Высота')
+
+    def __str__(self):
+        return f"{self.image}"
