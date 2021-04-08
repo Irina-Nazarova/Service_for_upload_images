66ba5a977dbcf25e24a4be061d3a52ecfdb1a281
Irina-Nazarova <Irina.g.nazarova@yandex.ru>
2021-04-08 22:23:03 +0300

    added views

--- images/views.py
+++ images/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from images.forms import ImageCreate
+from images.models import Image
+
+
+def index(request):
+    set_images = Image.objects.all()
+    img_list = "Список изображений"
+    img_add = "Добавить изображение"
+    img_not_found = "Нет доступных изображений"
+    return render(
+        request, "index.html", {"set_images": set_images,
+                                "img_list": img_list,
+                                "img_add": img_add,
+                                "img_not_found": img_not_found,},)
+
+
+def image_create(request):
+    pass
+
+
+def image_edit(request):
+    pass
