39caa661e40db8dd6753063915dffed5e340d7a3
Irina-Nazarova <Irina.g.nazarova@yandex.ru>
2021-04-12 21:50:38 +0300

    fixed limit_value images

--- images/models.py
+++ images/models.py
@@ -3,22 +3,36 @@ from django.db import models
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='',
-                                   blank=True,
-                                   null=True,
-                                   verbose_name='Файл изображения', )
+    image = models.ImageField(
+        upload_to="",
+        blank=True,
+        null=True,
+        verbose_name="Файл изображения",
+    )
     width = models.IntegerField(
-        blank=True, null=True,
-        validators=[MinValueValidator(limit_value=0),
-                    MaxValueValidator(limit_value=5000,
-                                      message="Максимально возможная ширина - 5000", )],
-        verbose_name='Ширина')
+        blank=True,
+        null=True,
+        validators=[
+            MinValueValidator(limit_value=0),
+            MaxValueValidator(
+                limit_value=7680,
+                message="Максимально возможная ширина - 7680",
+            ),
+        ],
+        verbose_name="Ширина",
+    )
     height = models.IntegerField(
-        blank=True, null=True,
-        validators=[MinValueValidator(limit_value=0),
-                    MaxValueValidator(limit_value=5000,
-                                      message="Максимально возможная высота - 5000", )],
-        verbose_name='Высота')
+        blank=True,
+        null=True,
+        validators=[
+            MinValueValidator(limit_value=0),
+            MaxValueValidator(
+                limit_value=4320,
+                message="Максимально возможная высота - 4320",
+            ),
+        ],
+        verbose_name="Высота",
+    )
 
     def __str__(self):
         return f"{self.image}"
